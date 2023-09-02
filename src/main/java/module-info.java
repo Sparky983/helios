572d8808c0cf9c6c04e3f1965a20671b60d483ee
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-09-02 18:55:04 +1000

    Fully remove cf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ import me.sparky983.helios.annotations.Experimental;
 @SuppressWarnings("JavaModuleNaming")
 @Experimental // 0.x.y
 module me.sparky983.helios {
-  requires static org.checkerframework.checker.qual;
   requires static java.compiler; // for Javadocs
 
   exports me.sparky983.helios.optional;
