39acbab4b20b8a11dc3321fb3344e651884c3de6
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-09-02 19:18:34 +1000

    Add module-info.java Javadocs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,12 @@
 import me.sparky983.helios.annotations.Experimental;
 
+/**
+ * Contains the Helios API.
+ * <h2>Warning</h2>
+ * Helios has not fully released, and such, APIs may break between minor versions
+ *
+ * @since 0.1
+ */
 @SuppressWarnings("JavaModuleNaming")
 @Experimental // 0.x.y
 module me.sparky983.helios {
