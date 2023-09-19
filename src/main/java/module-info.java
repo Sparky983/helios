26304d9e36da8a0301f8e5515c7a19537a9f6a1b
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-09-19 22:08:03 +1000

    Correct grammar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ import me.sparky983.helios.annotations.Experimental;
 /**
  * Contains the Helios API.
  * <h2>Warning</h2>
- * Helios has not fully released, and such, APIs may break between minor versions
+ * Helios has not been fully released, and as such, APIs may break between minor versions
  *
  * @since 0.1
  */
